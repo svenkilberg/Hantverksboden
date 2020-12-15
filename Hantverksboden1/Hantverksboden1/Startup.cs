b8b1f68594a757dc2d141fc01c660d152a14535d
Sven Kilberg <sven.kilberg@gmail.com>
2020-12-15 08:19:12 +0100

    Connection string and services.AddDbContext added

--- Hantverksboden1/Hantverksboden1/Startup.cs
+++ Hantverksboden1/Hantverksboden1/Startup.cs
@@ -9,6 +9,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Hantverksboden1.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Hantverksboden1
 {
@@ -24,6 +25,9 @@ namespace Hantverksboden1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddControllersWithViews();
 
             services.AddScoped<ICraftRepository, MockCraftRepository>();
